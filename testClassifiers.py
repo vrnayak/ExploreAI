699b3f7ee7da5c1ee779886e622992e92e36e97c
vrnayak <vrnayak@umich.edu>
2020-06-16 21:27:48 -0400

    Added testing mechanism for preloaded data

--- testClassifiers.py
+++ testClassifiers.py
@@ -5,6 +5,7 @@ import numpy as np
 import idx2numpy as idx
 from Classifiers import SimpleNeuralClassifier
 
+# Loads images & labels from MNIST Database
 def load_mnist_data(imgPath, lblPath):
 
 	examples = idx.convert_from_file(imgPath)
@@ -15,23 +16,36 @@ def load_mnist_data(imgPath, lblPath):
 	data = np.concatenate((examples, labels), axis=1)
 	return data
 
-def run_mnist_network():
+# If preload is True, weights & biases are preloaded instead of trained
+def run_mnist_network(preload, filename = None):
 
-	mnistTrainingSet = 'Datasets/mnistTrainingImgs-60k-idx1-ubyte'
-	mnistTrainingLbls = 'Datasets/mnistTrainingLbls-60k-idx1-ubyte'
+	nn = SimpleNeuralClassifier(layers = [784, 20, 10], eta = 1.0, batchSize = 50, epochs = 30)
+
+	if preload is True:
+		nn.load(filename)
+		print('Weights & biases have successfully been loaded from file.')
+	else:
+		mnistTrainingSet = 'Datasets/mnistTrainingImgs-60k-idx1-ubyte'
+		mnistTrainingLbls = 'Datasets/mnistTrainingLbls-60k-idx1-ubyte'
+		trainingData = load_mnist_data(mnistTrainingSet, mnistTrainingLbls)
+
+		# Train Neural Network & Save Weights/Biases
+		nn.train(trainingData)
+		nn.save('mnistNetworkInfo-784-20-10.csv')
+
+	# Test Neural Network
 	mnistTestingSet = 'Datasets/mnistTestingImgs-10k-idx1-ubyte'
 	mnistTestingLbls = 'Datasets/mnistTestingLbls-10k-idx1-ubyte'
-
-	trainingData = load_mnist_data(mnistTrainingSet, mnistTrainingLbls)
 	testingData = load_mnist_data(mnistTestingSet, mnistTestingLbls)
 
-	nn = SimpleNeuralClassifier(layers = [784, 20, 10], eta = 1.0,
-								batchSize = 50, epochs = 30)
-	nn.train(trainingData)
-	nn.test(testingData)
-	nn.save('mnistNetworkInfo-784-20-10.csv')
+	accuracy = nn.test(testingData)
+	print('The neural network correctly classified {:.2f}% of all images.'.format(accuracy * 100))
+
 
 if __name__ == '__main__':
+	
+	# Important files
+	networkInfo = 'mnistNetworkInfo-784-20-10.csv'
 
 	# Train & test neural network to classify handwritten digits from MNIST Database
-	run_mnist_network()
+	run_mnist_network(True, networkInfo)
